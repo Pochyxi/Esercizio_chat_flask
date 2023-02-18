00bbb26df8737edb33e147d09c9b4c728888de5f
Pochyxi <adienerlopez@gmail.com>
2023-02-18 14:52:35 +0100

    updt

--- ChatAPP/__init__.py
+++ ChatAPP/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_login import LoginManager
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = 'My secret key'
+app.secret_key = os.getenv('APP_SECRET_KEY')
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
