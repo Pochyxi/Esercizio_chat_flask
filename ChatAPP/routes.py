5ee8ea5f8ed4fa2f74ffe5968d7ffd7dc887de34
Pochyxi <adienerlopez@gmail.com>
2023-02-18 01:15:08 +0100

    prima versione terminata, funzionalità di base

--- ChatAPP/routes.py
+++ ChatAPP/routes.py
@@ -8,6 +8,12 @@ from ChatAPP.db import get_rooms_for_user, save_user, get_user, save_room, add_r
 from ChatAPP.forms import MessageForm
 
 
+def check_auth():
+    if current_user.is_authenticated:
+        return True
+    return False
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if not current_user.is_authenticated:
@@ -21,8 +27,10 @@ def home():
             # pass # do something else
             print("logout")
             return redirect(url_for('logout'))
+        elif request.form.get('add_room') == 'add_room':
+            return redirect(url_for('create_room'))
 
-    return render_template('index.html', rooms=rooms)
+    return render_template('index.html', rooms=rooms, check_auth=check_auth())
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -32,15 +40,18 @@ def login():
 
     message = ''
     if request.method == 'POST':
-        username = request.form.get('username')
-        password_input = request.form.get('password')
-        user = get_user(username)
+        if request.form.get('username') and request.form.get('password'):
+            username = request.form.get('username')
+            password_input = request.form.get('password')
+            user = get_user(username)
 
-        if user and user.check_password(password_input):
-            login_user(user)
-            return redirect(url_for('home'))
+            if user and user.check_password(password_input):
+                login_user(user)
+                return redirect(url_for('home'))
+            else:
+                message = "Autenticazione fallita"
         else:
-            message = "Autenticazione fallita"
+            message = "Non puoi lasciare i campi vuoti"
 
         if request.form.get('registrati') == 'registrati':
             # pass # do something else
@@ -57,14 +68,17 @@ def signup():
 
     message = ''
     if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        try:
-            save_user(username, email, password)
-            return redirect(url_for('home'))
-        except DuplicateKeyError:
-            message = "Username già esistente"
+        if request.form.get('username') and request.form.get('email') and request.form.get('password'):
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            try:
+                save_user(username, email, password)
+                return redirect(url_for('home'))
+            except DuplicateKeyError:
+                message = "Username già esistente"
+        else:
+            message = 'Non lasciare i campi vuoti'
     return render_template('signup.html', message=message)
 
 
@@ -80,20 +94,21 @@ def logout():
 def create_room():
     message = ''
     if request.method == 'POST':
-        room_name = request.form.get('room_name')
-        usernames = [username.strip() for username in request.form.get('members').split(',')]
+        if request.form.get('room_name'):
+            room_name = request.form.get('room_name')
+            usernames = [username.strip() for username in request.form.get('members').split(',')]
 
-        if len(room_name) and len(usernames):
-            room_id = save_room(room_name, current_user.username)
+            if len(room_name) and len(usernames):
+                room_id = save_room(room_name, current_user.username)
 
-            if current_user.username in usernames:
-                usernames.remove(current_user.username)
-                add_room_members(room_id, room_name, usernames, current_user.username)
-                redirect(url_for('view_room', room_id=room_id))
-            else:
-                message = "Errore nella creazione della stanza"
+                if current_user.username in usernames:
+                    usernames.remove(current_user.username)
+                    add_room_members(room_id, room_name, usernames, current_user.username)
+            return redirect(url_for('home'))
+        else:
+            message = "Errore nella creazione della stanza"
 
-    return render_template('create_room.html', message=message)
+    return render_template('create_room.html', message=message, check_auth=check_auth())
 
 
 @app.route('/rooms/<room_id>/edit', methods=['GET', 'POST'])
@@ -106,27 +121,33 @@ def edit_room(room_id):
 
         message = ''
         if request.method == 'POST':
-            room_name = request.form.get('room_name')
-            room['name'] = room_name
-            update_room(room_id, room_name)
+            if request.form.get('room_name') and request.form.get('members'):
+                room_name = request.form.get('room_name')
+                room['name'] = room_name
+                update_room(room_id, room_name)
 
-            new_members = [username.strip() for username in request.form.get('members').split(',')]
+                new_members = [username.strip() for username in request.form.get('members').split(',')]
 
-            members_to_add = set(new_members) - set(existing_room_members)
-            members_to_remove = list(set(existing_room_members) - set(new_members))
+                members_to_add = set(new_members) - set(existing_room_members)
+                members_to_remove = list(set(existing_room_members) - set(new_members))
 
-            if len(members_to_add):
-                add_room_members(room_id, room_name, members_to_add, current_user.username)
+                if len(members_to_add):
+                    add_room_members(room_id, room_name, members_to_add, current_user.username)
 
-            if len(members_to_remove):
-                remove_room_members(room_id, members_to_remove)
-            message = 'Stanza modificata con successo'
-            room_members_str = ",".join(new_members)
-            room['name'] = get_room(room_id)
-            return redirect(url_for('view_room', room_id=room_id))
-
+                if len(members_to_remove):
+                    remove_room_members(room_id, members_to_remove)
+                message = 'Stanza modificata con successo'
+                room_members_str = ",".join(new_members)
+                room['name'] = get_room(room_id)
+                return redirect(url_for('view_room', room_id=room_id))
+            elif request.form.get('go_home') == 'home':
+                return redirect(url_for('home'))
         room_members_str = ",".join(existing_room_members)
-        return render_template('edit_room.html', room=room, room_members_str=room_members_str, message=message)
+        return render_template('edit_room.html',
+                               room=room,
+                               room_members_str=room_members_str,
+                               message=message,
+                               check_auth=check_auth())
     else:
         return "Stanza non trovata", 404
 
@@ -139,17 +160,19 @@ def view_room(room_id):
     room_messages = get_message_by_room_id(room_id)
     room_messages_length = len(room_messages)
     room_members = get_room_members(room_id)
+    check_room_admin = is_room_admin(room_id, current_user.username)
+
     if request.method == 'POST':
         if request.form.get('message_input'):
             save_message(room_id, current_user.username, message_form.message.data)
             print("inserito messaggio con ", message_form.message.data)
             return redirect(url_for('view_room', room_id=room_id))
         elif request.form.get('modifica_stanza') == 'Modifica stanza':
-            print("eccomi")
             print(request.path)
             return redirect(request.path + "/edit")
             # ritornare la rotta attuale più /edit
-            # return redirect()
+        elif request.form.get('go_home') == 'home':
+            return redirect(url_for('home'))
 
     if room and is_room_member(room_id, current_user.username):
         return render_template('view_room.html',
@@ -159,7 +182,9 @@ def view_room(room_id):
                                room_members=room_members,
                                message_form=message_form,
                                room_messages=room_messages,
-                               room_messages_length=room_messages_length)
+                               room_messages_length=room_messages_length,
+                               check_room_admin=check_room_admin,
+                               check_auth=check_auth())
     else:
         return "Stanza non trovata", 404
 
